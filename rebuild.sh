385f42496b71cea8435ddd487a59550c37c07ad4
Kostyantin Savchuk <kostya@gyry.com.ua>
2020-08-03 15:16:04 +0300

    add rebuild.sh

---
+++ rebuild.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+echo "### BUILD U-BOOT"
+cd u-boot
+./make.sh rk3399
+cd ..
+
+echo "### BUILD KERNEL"
+cd kernel
+make rockchip_defconfig
+make rk3399-rockpi-4b.img -j$(nproc)
+cd ..
+
+echo "### BUILD AOSP"
+source build/envsetup.sh
+lunch rk3399_box-userdebug
+make -j$(nproc)
+
+echo "### GENERATE IMAGES"
+./mkimage.sh
+cd rockdev
+#./mkupdate.sh
+./android-gpt.sh
+cp /mnt/rockpi4-android9/rockdev/Image/gpt.img /media/psf/git/ && sync
